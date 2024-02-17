53986d2502d7abdef997b5c7ffb84c7ad56d560f
Raviteja <120414406+raviteja-02@users.noreply.github.com>
2024-02-17 08:52:49 +0530

    first commit

---
+++ static/js/main.js
@@ -0,0 +1,11 @@
+document.querySelector('.my-header').addEventListener('click', function() {
+    alert('You clicked the header!');
+});
+
+document.querySelector('.my-subheader').addEventListener('click', function() {
+    alert('You clicked the subheader!');
+});
+
+document.querySelector('.my-image').addEventListener('click', function() {
+    alert('You clicked the image!');
+});
